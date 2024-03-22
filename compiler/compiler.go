d36996743425c2acc6b15c6ef99c18fbab4bbd9c
Nathan4044 <nathan4044@gmail.com>
2024-03-22 17:42:17 +0000

    Add initial compiler
    first Opcode
    first compiler instruction

---
+++ compiler/compiler.go
@@ -0,0 +1,88 @@
+// The compiler package contains the definition of the Compiler type, which
+// compiles an AST node into Bytecode
+package compiler
+
+import (
+    "lisp/ast"
+	"lisp/code"
+	"lisp/object"
+)
+
+// The Compiler is a struct that holds the result of calls to the Compile
+// method
+type Compiler struct {
+    instructions code.Instructions // instructions generated from Compile
+    constants []object.Object // constant expressions found during Compile
+}
+
+// Bytecode is a struct containing the instructions produced by a Compiler and
+// returned by the Bytecode method
+type Bytecode struct {
+    Instructions code.Instructions
+    Constants []object.Object
+}
+
+// Return the address of a new Compiler instance
+func New() *Compiler {
+    return &Compiler{
+        instructions: code.Instructions{},
+        constants: []object.Object{},
+    }
+}
+
+// Compile an AST Expression into bytecode instructions. Return an error if there is
+// a problem during the compilation step
+func (c *Compiler) Compile(expr ast.Expression) error {
+    switch expr := expr.(type) {
+    case *ast.Program:
+        for _, e := range expr.Expressions {
+            err := c.Compile(e)
+
+            if err != nil {
+                return err
+            }
+        }
+    case *ast.SExpression:
+        for _, e := range expr.Args {
+            err := c.Compile(e)
+
+            if err != nil {
+                return err
+            }
+        }
+    case *ast.IntegerLiteral:
+        integer := &object.Integer{Value: expr.Value}
+
+        c.emit(code.OpConstant, c.addConstant(integer))
+    }
+
+    return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+    return &Bytecode{
+        Instructions: c.instructions,
+        Constants: c.constants,
+    }
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+    c.constants = append(c.constants, obj)
+
+    return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+    ins := code.Make(op, operands...)
+    pos := c.addInstruction(ins)
+
+    return pos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+    posNewInstruction := len(c.instructions)
+
+    c.instructions = append(c.instructions, ins...)
+
+    return posNewInstruction
+}
---
+++ compiler/compiler_test.go
@@ -0,0 +1,134 @@
+package compiler
+
+import (
+	"fmt"
+	"lisp/ast"
+	"lisp/code"
+	"lisp/lexer"
+	"lisp/object"
+	"lisp/parser"
+	"slices"
+	"testing"
+)
+
+type compilerTestCase struct {
+    input string
+    expectedConstants []interface{}
+    expectedInstructions []code.Instructions
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+    tests := []compilerTestCase{
+        {
+            input: "(+ 1 2)",
+            expectedConstants: []interface{}{1, 2},
+            expectedInstructions: []code.Instructions{
+                code.Make(code.OpConstant, 0),
+                code.Make(code.OpConstant, 1),
+            },
+        },
+    }
+
+    runCompilerTests(t, tests)
+}
+
+func runCompilerTests(t *testing.T, tests []compilerTestCase) {
+    t.Helper()
+
+    for _, tt := range tests {
+        program := parse(tt.input)
+        compiler := New()
+        
+        err := compiler.Compile(program)
+
+        if err != nil {
+            t.Fatalf("compiler error: %s", err)
+        }
+
+        bytecode := compiler.Bytecode()
+
+        err = testInstructions(tt.expectedInstructions, bytecode.Instructions)
+
+        if err != nil {
+            t.Fatalf("testInstructions failed: %s", err)
+        }
+
+        err = testConstants(tt.expectedConstants, bytecode.Constants)
+
+        if err != nil {
+            t.Fatalf("testConstants failed: %s", err)
+        }
+    }
+}
+
+func parse(input string) *ast.Program {
+    l := lexer.New(input)
+    p := parser.New(l)
+
+    return p.ParseProgram()
+}
+
+func testInstructions(
+    expected []code.Instructions,
+    actual code.Instructions,
+) error {
+    concatted := slices.Concat(expected...)
+
+    if len(actual) != len(concatted) {
+        return fmt.Errorf(
+            "wrong instruction length:\n  want=%q\n  got=%q",
+            concatted, actual,
+        )
+    }
+
+    for i, ins := range concatted {
+        if actual[i] != ins {
+            return fmt.Errorf(
+                "wrong instruction at %d:\n  want=%q\n  got=%q", 
+                i, concatted, actual,
+            )
+
+        }
+    }
+
+    return nil
+}
+
+func testConstants(
+    expected []interface{},
+    actual []object.Object,
+) error {
+    if len(actual) != len(expected) {
+        return fmt.Errorf(
+            "wrong instruction length:\n  want=%q\n  got=%q",
+            expected, actual,
+        )
+    }
+
+    for i, constant := range expected {
+        switch constant := constant.(type) {
+        case int:
+            err := testIntegerObject(int64(constant), actual[i])
+
+            if err != nil {
+                return fmt.Errorf("constant %d - testIntegerObject failed: %s", i, err)
+            }
+        }
+    }
+
+    return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+    result, ok := actual.(*object.Integer)
+
+    if !ok {
+        return fmt.Errorf("object is not Integer: got=%T(%+v)", actual, actual)
+    }
+
+    if result.Value != expected {
+        return fmt.Errorf("object has wrong value: got=%d want=%d", result.Value, expected)
+    }
+
+    return nil
+}
