529c3a5ab491ec6574f1d2e5e0f30570db470c94
Nathan Boswell <nathan4044@gmail.com>
2024-04-05 15:27:05 +0100

    Redesign repl and main interaction

--- main.go
+++ main.go
@@ -2,44 +2,100 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"lisp/interpreter"
+	"lisp/compiler"
+	"lisp/evaluator"
+	"lisp/lexer"
 	"lisp/object"
+	"lisp/parser"
 	"lisp/repl"
+	"lisp/vm"
 	"os"
 )
 
+var engine *string = flag.String("engine", "vm", "enter 'vm' or 'eval'")
+
 func main() {
-	switch len(os.Args) {
+	flag.Parse()
+	fmt.Printf("%+v\n", flag.Args())
+	switch len(flag.Args()) {
 	// if there are no args provided, evaluate from stdin
-	case 1:
-		repl.Start(os.Stdin, os.Stdout)
+	case 0:
+		if *engine == "eval" {
+			repl.Start(os.Stdin, os.Stdout)
+		} else {
+			repl.StartCompiled(os.Stdin, os.Stdout)
+		}
 		// if a filename is provided, evaluate the code within the file
-	case 2:
-		fileContents, err := os.ReadFile(os.Args[1])
+	case 1:
+		// Currently, execution of only one file is supported.
+		// There are also no command options.
+		fileContents, err := os.ReadFile(flag.Arg(0))
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, err.Error())
 			return
 		}
 
-		env := object.NewEnvironment(nil)
-		result, errors := interpreter.Run(string(fileContents), env)
-
-		if len(errors) > 0 {
-			for _, err := range errors {
-				fmt.Fprintf(os.Stderr, err)
-			}
+		if *engine == "eval" {
+			runFile(string(fileContents))
 		} else {
-			err, ok := result.(*object.ErrorObject)
-
-			if ok {
-				fmt.Fprintf(os.Stderr, err.Inspect())
-			}
+			runCompiled(string(fileContents))
 		}
-		// Currently, execution of only one file is supported.
-		// There are also no command options.
 	default:
 		fmt.Fprintf(os.Stderr, "expected only 1 filename")
 	}
+}
+
+// Convert the provided program into an AST, then evluate it.
+func runFile(source string) {
+	l := lexer.New(source)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if len(p.Errors) > 0 {
+		for _, err := range p.Errors {
+			fmt.Fprintf(os.Stderr, err)
+		}
+
+		return
+	}
+
+	env := object.NewEnvironment(nil)
+	result := evaluator.Evaluate(program, env)
+
+	fmt.Println(result.Inspect())
+}
+
+// Compile the expressions in the provided program into bytecode, then
+// execute the bytecode on a VM.
+func runCompiled(source string) {
+	l := lexer.New(source)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if len(p.Errors) > 0 {
+		for _, err := range p.Errors {
+			fmt.Fprintf(os.Stderr, err)
+		}
+
+		return
+	}
+
+	c := compiler.New()
+	err := c.Compile(program)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "compiler error: %s", err)
+	}
+
+	v := vm.New(c.Bytecode())
+	err = v.Run()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "vm error: %s", err)
+	}
+
+	fmt.Println(v.LastPoppedStackElem())
 }
--- repl/repl.go
+++ repl/repl.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"io"
 	"lisp/compiler"
-	"lisp/interpreter"
+	"lisp/evaluator"
+	"lisp/lexer"
 	"lisp/object"
+	"lisp/parser"
 	"lisp/vm"
 )
 
@@ -17,7 +19,41 @@ const PROMPT = ">>> "
 // with stdin and stdout as the Reader and Writer.
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	//env := object.NewEnvironment(nil)
+	env := object.NewEnvironment(nil)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		if len(scanner.Text()) == 0 {
+			continue
+		}
+
+		l := lexer.New(scanner.Text())
+		p := parser.New(l)
+		program := p.ParseProgram()
+
+		if len(p.Errors) > 0 {
+			for _, err := range p.Errors {
+				fmt.Fprintf(out, err)
+			}
+
+			return
+		}
+
+		result := evaluator.Evaluate(program, env)
+		fmt.Fprintln(out, result.Inspect())
+	}
+}
+
+// Starts an interactive interpreter, conventionally in the terminal
+// with stdin and stdout as the Reader and Writer.
+func StartCompiled(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
 	constants := []object.Object{}
 	globals := make([]object.Object, vm.GlobalSize)
 	symbolTable := compiler.NewSymbolTable()
@@ -38,15 +74,34 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		//result, errors := interpreter.Run(scanner.Text(), env)
-		result, errors := interpreter.RunCompiled(scanner.Text(), &constants, symbolTable, globals)
+		l := lexer.New(scanner.Text())
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-		if len(errors) > 0 {
-			for _, err := range errors {
-				fmt.Fprintln(out, err)
+		if len(p.Errors) > 0 {
+			for _, err := range p.Errors {
+				fmt.Fprintf(out, err)
 			}
-		} else {
-			fmt.Fprintln(out, result.Inspect())
+
+			return
 		}
+
+		c := compiler.NewWithState(constants, symbolTable)
+		err := c.Compile(program)
+
+		if err != nil {
+			fmt.Fprintf(out, "compiler error: %s", err)
+		}
+
+		v := vm.NewWithState(c.Bytecode(), globals)
+		err = v.Run()
+
+		if err != nil {
+			fmt.Fprintf(out, "vm error: %s", err)
+		}
+
+		result := v.LastPoppedStackElem()
+
+		fmt.Fprintln(out, result.Inspect())
 	}
 }
